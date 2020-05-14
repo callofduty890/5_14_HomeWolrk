c389aac593af210a0d8e1d1cc6ef809204186c8a
俊华 蔡 <1032031912@qq.com>
2020-05-14 09:01:20 +0800

    索引器访问不同名变量

--- 5_14_代码/Program.cs
+++ 5_14_代码/Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Person;
 namespace _5_14_代码
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Person.Person p = new Person.Person();
+
+            //使用索引器的方式给两个属性赋值
+            p[0] = "callofduty"; //账号赋值
+            p[1] = "123456";     //密码赋值
+            //获取属性的值
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Password);
+            //通过索引器取值
+            Console.WriteLine(p[0]);
+            Console.WriteLine(p[1]);
+            Console.ReadKey();
         }
     }
 }
---
+++ Person/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        //定义两个字段
+        private string _name;
+        private string _password;
+
+        //属性：1,用户名 2.密码
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        //定义索引器
+        public string this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return Name;
+                }
+                else if (index == 1)
+                {
+                    return Password;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (index == 0)
+                {
+                    Name = value;
+                }
+                else if (index == 1)
+                {
+                    Password = value;
+                }
+            }
+        }
+    }
+}
