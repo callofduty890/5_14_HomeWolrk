993708440463e4aaf4429bb9b19d9deeb179b30a
俊华 蔡 <1032031912@qq.com>
2020-05-14 09:47:07 +0800

    完成索引器的重载

--- 5_14_代码/Program.cs
+++ 5_14_代码/Program.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Person;
+using Animal;
+
 namespace _5_14_代码
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Person.Person p = new Person.Person();
+            #region 索引器使用方法一
+            //Person.Person p = new Person.Person();
 
-            //使用索引器的方式给两个属性赋值
-            p[0] = "callofduty"; //账号赋值
-            p[1] = "123456";     //密码赋值
-            //获取属性的值
-            Console.WriteLine(p.Name);
-            Console.WriteLine(p.Password);
-            //通过索引器取值
-            Console.WriteLine(p[0]);
-            Console.WriteLine(p[1]);
+            ////使用索引器的方式给两个属性赋值
+            //p[0] = "callofduty"; //账号赋值
+            //p[1] = "123456";     //密码赋值
+            ////获取属性的值
+            //Console.WriteLine(p.Name);
+            //Console.WriteLine(p.Password);
+            ////通过索引器取值
+            //Console.WriteLine(p[0]);
+            //Console.WriteLine(p[1]);
+            #endregion
+
+            Animal.Animal an = new Animal.Animal();
+            //使用第一个索引
+            an["Fish"] = 20;
+            an["Tiger"] = 30;
+            an["Mouse"] = 5;
+
+            Console.WriteLine(an["Fish"]);
+            Console.WriteLine(an["Tiger"]);
+            Console.WriteLine(an["Tiger"]);
+
+            Console.WriteLine("=========================");
+
+            //第二个索引器
+            an[1] = "dog";
+            an[2] = "cat";
+            an[3] = "bird";
+
+            Console.WriteLine(an[1]);
+            Console.WriteLine(an[2]);
+            Console.WriteLine(an[3]);
+
             Console.ReadKey();
         }
     }
---
+++ Animal/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Animal
+{
+    public class Animal
+    {
+        //字典类型变量  键-值
+        private Dictionary<string, int> indexer1 = new Dictionary<string, int>();
+
+        //字符串类型的数组
+        private string[] indexer2 = new string[10];
+
+        //索引器的重载
+
+        //设计索引器- indexer1
+        public int this[string index]
+        {
+            get
+            {
+                //查找是否有此索引
+                if (indexer1.ContainsKey(index))
+                {
+                    return indexer1[index];
+                }
+                else
+                {
+                    throw new Exception("未找到相应的值");
+                }
+            }
+            set
+            {
+                //判断是否找到当前的索引值
+                if (indexer1.ContainsKey(index))
+                {
+                    indexer1[index] = value;
+                }
+                else
+                {
+                    //添加对应的键与值
+                    indexer1.Add(index, value);
+                }
+            }
+        }
+
+        //设计索引器 - indexer2
+        public string this[int index]
+        {
+            get
+            {
+                return indexer2[index];
+            }
+            set
+            {
+                indexer2[index] = value;
+            }
+        }
+
+
+    }
+}
